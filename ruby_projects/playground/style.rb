50f97cfb394982393ec11160e9b6e377e4f3454a
Caique Bertolozzi <caiquebb@gmail.com>
2021-04-17 13:57:29 -0300

    Ruby Style Guide

---
+++ ruby_projects/playground/style.rb
@@ -0,0 +1,23 @@
+# Style
+
+# Use snake_case for variables and methods
+
+this_is_snake_case
+
+also_snake_case
+
+def my_name_is
+  # tab size with two spaces
+end
+
+# Use PascalCase for class name
+
+PascalCase
+
+camelCase
+
+class SomeClass
+
+end
+
+# Ruby Style Guide: https://rubystyle.guide
