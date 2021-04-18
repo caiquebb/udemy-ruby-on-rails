0a830308f6e8af52224a364b6c62c71a49291753
Caique Bertolozzi <caiquebb@gmail.com>
2021-04-18 20:56:30 -0300

    Use self

--- ruby_projects/playground/crud_module.rb
+++ ruby_projects/playground/crud_module.rb
@@ -3,15 +3,15 @@ module Crud
 
   puts "Module CRUD activated"
   
-  def Crud.create_hash_digest(password)
+  def self.create_hash_digest(password)
     BCrypt::Password.create(password)
   end
   
-  def Crud.verify_hash_digest(password)
+  def self.verify_hash_digest(password)
     BCrypt::Password.new(password)
   end
   
-  def Crud.create_secure_users(list_of_users)
+  def self.create_secure_users(list_of_users)
     list_of_users.each do |user|
       user[:password] = create_hash_digest(user[:password])
     end
@@ -19,7 +19,7 @@ module Crud
     list_of_users
   end
   
-  def Crud.authenticate_user(username, password, list_of_users)
+  def self.authenticate_user(username, password, list_of_users)
     list_of_users.each do |user|
       if user[:username] == username && verify_hash_digest(user[:password]) == password
         return user
