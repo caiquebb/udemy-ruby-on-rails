4041149ae14cb7e6ef27c2ac9e7e474fdf5c491f
Caique Bertolozzi <caiquebb@gmail.com>
2021-04-18 18:13:18 -0300

    Bcrypt

--- ruby_projects/playground/oop_proojects/student.rb
+++ ruby_projects/playground/oop_proojects/student.rb
@@ -3,12 +3,6 @@
 class Student
   attr_accessor :first_name, :last_name, :email, :username
 
-  # @first_name
-  # @last_name
-  # @email
-  # @username
-  # @password
-
   def initialize(first_name, last_name, email, username, password)
     @first_name = first_name
     @last_name = last_name
@@ -17,32 +11,10 @@ class Student
     @password = password
   end
 
-  # def set_username
-  #   @username = "caiquebertolozzi"
-  # end
-
-  def first_name=(name)
-    @first_name = name
-  end
-  
-  # def first_name
-  #   @first_name
-  # end
-
   def to_s
     "First name: #{@first_name}, Last name: #{last_name}, Username: #{username}, email: #{email}"
   end
 end
-
-# caique = Student.new
-# caique.first_name = "Caique"
-# caique.last_name = "Bertolozzi"
-# caique.email = "caiquebb@gmail.com"
-# caique.set_username
-# puts caique.first_name
-# puts caique.last_name
-# puts caique.email
-# puts caique.username
 
 caique = Student.new("Caique", "Bertolozzi", "caique@bertolozzi.me", "caiquebertolozzi", "123456")
 john = Student.new("John", "Doe", "john@doe.me", "johndoe", "123456")
@@ -53,3 +25,17 @@ puts john
 caique.last_name = john.last_name
 
 puts caique
+
+require 'bcrypt'
+
+my_password = BCrypt::Password.create("my password")
+#=> "$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey"
+
+p my_password.version              #=> "2a"
+p my_password.cost                 #=> 12
+p my_password == "my password"     #=> true
+p my_password == "not my password" #=> false
+
+# my_password = BCrypt::Password.new("$2a$12$K0ByB.6YI2/OYrB4fQOYLe6Tv0datUVf6VZ/2Jzwm879BW5K1cHey")
+# my_password == "my password"     #=> true
+# my_password == "not my password" #=> false
